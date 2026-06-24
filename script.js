5d42f55e1f4241bb8b3f30b8d38e7ee4a0b8a53a
csmueed <csmueed@gmail.com>
2026-06-24 22:04:22 +0500

    calculator project started

---
+++ script.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll(".box");
+const input = document.querySelector(".data");
+
+buttons.forEach((button)=>{
+    button.addEventListener("click", (e)=>{
+        e.preventDefault();
+
+    })
+})
