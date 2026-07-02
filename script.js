d6d192baae82e9f50cb12475c10f1670072a6a29
csmueed <csmueed@gmail.com>
2026-07-02 20:19:23 +0500

    a good logical basic calculator is ready

--- script.js
+++ script.js
@@ -5,36 +5,96 @@ let firstNumber = "";
 let secondNumber = "";
 let Operator = "";
 let result = "";
+let firstTurn = true;
 
 let allOperators = ["-", "+", "*", "/"];
 let allNumbers = [".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    if (allNumbers.includes(button.textContent) && firstNumber === "") {
+    if (
+      allNumbers.includes(button.textContent) &&
+      Operator === "" &&
+      firstTurn === true
+    ) {
       display.value += button.textContent;
-    } else if (allOperators.includes(button.textContent)) {
-      firstNumber = display.value;
+      firstNumber += button.textContent;
+    } else if (
+      allOperators.includes(button.textContent) &&
+      firstNumber !== "" &&
+      secondNumber === "" &&
+      Operator === "" &&
+      firstTurn === true
+    ) {
       display.value = "";
       Operator = button.textContent;
-    } else if (allNumbers.includes(button.textContent) && firstNumber !== "") {
+    } else if (
+      allNumbers.includes(button.textContent) &&
+      Operator !== "" &&
+      firstNumber !== "" &&
+      firstTurn === true
+    ) {
       display.value += button.textContent;
       secondNumber += button.textContent;
-    } else if (button.textContent === "=") {
-      result = "";
+    } else if (
+      button.textContent === "=" &&
+      firstNumber !== "" &&
+      secondNumber !== "" &&
+      Operator !== ""
+    ) {
       display.value = "";
       result = calculate(Number(firstNumber), Number(secondNumber), Operator);
       display.value = result;
       firstNumber = "";
       secondNumber = "";
       Operator = "";
-      firstNumber = result;
+      firstNumber = Number(result);
+      firstTurn = false;
+    } else if (
+      allOperators.includes(button.textContent) &&
+      firstTurn === false &&
+      Operator === "" &&
+      secondNumber === ""
+    ) {
+      display.value = "";
+      Operator = button.textContent;
+    } else if (
+      allNumbers.includes(button.textContent) &&
+      firstTurn === false &&
+      Operator !== "" &&
+      firstNumber != ""
+    ) {
+      secondNumber += button.textContent;
+      display.value = secondNumber;
+    } else if (
+      allOperators.includes(button.textContent) &&
+      firstNumber !== "" &&
+      secondNumber !== "" &&
+      Operator !== ""
+    ) {
+      display.value = "";
+      result = calculate(Number(firstNumber), Number(secondNumber), Operator);
+      display.value = result;
+      Operator = button.textContent;
+      firstNumber = "";
+      secondNumber = "";
+      firstNumber = Number(result);
       result = "";
+      firstTurn = false;
     } else if (button.textContent === "Clear") {
       clearAll();
     }
   });
 });
+
+function clearAll() {
+  display.value = "";
+  firstNumber = "";
+  secondNumber = "";
+  result = "";
+  firstTurn = true;
+  Operator = "";
+}
 
 function calculate(firstNumber, secondNumber, Operator) {
   if (Operator === "+") {
@@ -50,12 +110,4 @@ function calculate(firstNumber, secondNumber, Operator) {
     let result = firstNumber / secondNumber;
     return Number(result.toPrecision(2));
   }
-}
-
-function clearAll() {
-  firstNumber = "";
-  secondNumber = "";
-  Operator = "";
-  result = "";
-  display.value = "";
 }
